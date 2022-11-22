ffba570548cd6e652b1dfa6beba4b7230d05a06f
Michal Gorecki <michal.gorecki@iteo.com>
2022-11-22 18:51:16 +0100

    app/Http/Controllers/UsersCommentsController.php

--- app/Http/Controllers/UsersCommentsController.php
+++ app/Http/Controllers/UsersCommentsController.php
@@ -3,31 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class UsersCommentsController extends Controller
 {
-    public function index(): Factory|View|Application
+    public function index(Request $request): Factory|View|Application
     {
         /** @var string $by */
-        $by = request('by') ?? 'created_at';
+        $by = $request->input('by', 'created_at');
 
         /** @var string $sort */
-        $sort = request('sort') ?? 'DESC';
+        $sort = $request->input('sort', 'DESC');
 
-        $user = auth()->user();
+        /** @var User $user */
+        $user = $request->user();
 
-        if ($user) {
-            return view('user.comments', [
-                'comments' => Comment::filter(['search' => request(['search']), 'id' => $user->id])
-                    ->orderBy($by, $sort)
-                    ->paginate(25)
-                    ->onEachSide(1),
-            ]);
-        }
-
-        abort(404);
+        return view('user.comments', [
+            'comments' => Comment::filter(['search' => request(['search']), 'id' => $user->id])
+                ->orderBy($by, $sort)
+                ->paginate(25)
+                ->onEachSide(1),
+        ]);
     }
 }
