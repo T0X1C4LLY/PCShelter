af7cc6fcb04ff6628fbb67b4a0d2cfc2659651a7
Michal Gorecki <michal.gorecki@iteo.com>
2022-11-25 16:32:59 +0100

    app/Other/SteamInfo.php

--- app/Other/SteamInfo.php
+++ app/Other/SteamInfo.php
@@ -6,8 +6,6 @@ namespace App\Other;
 
 use App\Exceptions\SteamResponseException;
 use App\Models\Game;
-use App\Models\GameCategory;
-use App\Models\Genre;
 use Illuminate\Support\Facades\URL;
 use Ramsey\Uuid\Uuid;
 
@@ -39,6 +37,42 @@ class SteamInfo
      */
     public function getByGameId(int $gameId): array
     {
+        $gameData = $this->getDataFromSteam($gameId);
+        $game = Game::where('steam_appid', $gameId)->first() ?: $this->addGame($gameData['data']);
+
+        $info = $this->getSteamDataAsArray($gameData);
+        $info['reviews'] = $game->getBestAndGeneralReviews();
+
+        return $info;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function addGame(array $data): Game
+    {
+        $genres = array_column($data['genres'], 'description');
+        $categories = array_column($data['categories'], 'description');
+
+        $gameData = [
+            'id' => Uuid::uuid4(),
+            'steam_appid' => $data['steam_appid'],
+            'name' => $data['name'],
+            'categories' => $categories,
+            'genres' => $genres,
+            'release_date' => $data['release_date'],
+            'header_image' => $data['header_image'],
+        ];
+
+        return Game::create($gameData);
+    }
+
+    /**
+     * @throws SteamResponseException
+     * @throws \JsonException
+     */
+    public function getDataFromSteam(int $gameId): array
+    {
         /** @var string $data */
         $data = file_get_contents(env('STEAM_URL').$gameId);
 
@@ -52,98 +86,50 @@ class SteamInfo
             throw SteamResponseException::byInvalidResponse();
         }
 
-        $game = Game::where('steam_appid', $gameId)->first() ?: $this->addGame($gameData['data']);
+        return $gameData;
+    }
 
+    public function getSteamDataAsArray(array $gameData): array
+    {
         $info = [];
 
         foreach ($this->gameArrayKeys as $key) {
             $info[$key] = $gameData['data'][$key] ?? null;
         }
 
-        // Replace steam URl to PCShelter URL
-        $info['about_the_game'] = preg_replace(
-            '/("https:\/\/store.steampowered.com\/app)/',
-            '"'.URL::to('/games'),
-            $info['about_the_game']
-        );
-
-        // Delete game title from URL
-        $info['about_the_game'] = preg_replace(
-            '/(\/[a-zA-Z0-9^\w]*\/")( target="_blank")/',
-            '"',
-            $info['about_the_game']
-        );
-
-        if ($info['release_date']) {
-            $info['release_date'] = $info['release_date']['coming_soon'] ? 'Coming Soon' : $info['release_date']['date'];
-        }
-
-        $info['reviews'] = $game->getBestAndGeneralReviews();
-
+        $info['about_the_game'] = $this->replaceSteamUrl($info['about_the_game']);
+        $info['about_the_game'] = $this->deleteGameTitle($info['about_the_game']);
+        $info['release_date'] = $this->setReleaseDate($info['release_date']);
 
         return $info;
     }
 
-    public function addGame(array $data): Game
+    public function replaceSteamUrl(string $text): string
     {
-        $genres = array_column($data['genres'], 'description');
-        $categories = array_column($data['categories'], 'description');
-
-        $genresInDb = Genre::pluck('name')->toArray();
-        $categoriesInDb = GameCategory::pluck('name')->toArray();
-
-        foreach ($genres as $genre) {
-            if (!in_array($genre, $genresInDb, true)) {
-                Genre::create([
-                    'id' => Uuid::uuid4(),
-                    'name' => $genre,
-                ]);
-
-                $this->addToFile($genre, 'genres.csv');
-            }
-        }
-
-        foreach ($categories as $category) {
-            $key = in_array($category, $categoriesInDb, true);
-            if ($key === false) {
-                GameCategory::create([
-                    'id' => Uuid::uuid4(),
-                    'name' => $category,
-                ]);
-
-                $this->addToFile($category, 'categories.csv');
-            }
-        }
-
-        $gameData = [
-            'id' => Uuid::uuid4(),
-            'steam_appid' => $data['steam_appid'],
-            'name' => $data['name'],
-            'categories' => $categories,
-            'genres' => $genres,
-            'release_date' => $data['release_date'],
-            'header_image' => $data['header_image'],
-        ];
-
-        /** @var string $gamesAsJson */
-        $gamesAsJson = file_get_contents(base_path().'/database/assets/games.json');
-        $gamesAsJson = substr($gamesAsJson, 0, -2);
-        $gamesAsJson .= ','.json_encode($gameData, JSON_THROW_ON_ERROR).']';
-
-        file_put_contents(
-            base_path().'/database/assets/games.json',
-            $gamesAsJson
+        /** @var string $changedText */
+        $changedText =  preg_replace(
+            '/("https:\/\/store.steampowered.com\/app)/',
+            '"'.URL::to('/games'),
+            $text
         );
 
-        return Game::create($gameData);
+        return $changedText;
     }
 
-    private function addToFile(string $data, string $file): void
+    public function deleteGameTitle(string $text): string
     {
-        file_put_contents(
-            base_path().'/database/assets/'.$file,
-            ','.$data,
-            FILE_APPEND
+        /** @var string $changedText */
+        $changedText = preg_replace(
+            '/(\/[a-zA-Z0-9^\w]*\/")( target="_blank")/',
+            '"',
+            $text
         );
+
+        return $changedText;
+    }
+
+    public function setReleaseDate(array $date): string
+    {
+        return $date['coming_soon'] ? 'Coming Soon' : $date['date'];
     }
 }
